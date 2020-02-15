7a64e031481b3848b26ec3ff373d62d8241cbcf7
JimmyLin39 <jimmylin0309@email.com>
2020-02-15 11:30:40 -0800

    change to render props

--- app/components/Tooltip.js
+++ app/components/Tooltip.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import withHover from './withHover'
+import Hover from './Hover'
 
 const styles = {
   container: {
@@ -24,19 +24,22 @@ const styles = {
   }
 }
 
-function Tooltip ({ text, children, hovering }) {
+export default function Tooltip({ text, children }) {
   return (
-    <div style={styles.container}>
-      {hovering === true && <div style={styles.tooltip}>{text}</div>}
-      {children}
-    </div>
+    <Hover>
+      {hovering => {
+        return (
+          <div style={styles.container}>
+            {hovering === true && <div style={styles.tooltip}>{text}</div>}
+            {children}
+          </div>
+        )
+      }}
+    </Hover>
   )
 }
 
 Tooltip.propTypes = {
   text: PropTypes.string.isRequired,
-  children: PropTypes.element,
-  hovering: PropTypes.bool.isRequired
+  children: PropTypes.element
 }
-
-export default withHover(Tooltip)
